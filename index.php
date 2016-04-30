e616716cc84228a8fb8c4c96302d2ad9cf8f43e8
lapaks <deepak.maharjan@outlook.com>
2016-04-30 12:07:15 -0400

    add

---
+++ index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Click Survey Solution</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width">
+    <link href="images/favicon.ico" rel="icon" type="image/x-icon" />
+	<link rel="stylesheet" href="css/style.css">
+    <script>
+		document.createElement("nav");
+		document.createElement("section");
+		document.createElement("header");
+		document.createElement("footer");
+		document.createElement("aside");
+    </script>
+</head>
+<body>
+<header id="header" class="clearfix">
+	<div id="logo">
+    	<a href="#"><img src="images/logo.png" alt="Click Survey Solution"></a>
+    </div>
+    <div class="phone-number">
+    	<span>01 (952)</span> 394-45-24
+    </div>
+    <nav>
+    	<ul>
+        	<li class="current-menu-item"><a href="#">Home</a></li>
+            <li><a href="#">Services</a>
+            	<ul class="sub-menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Our Works</a></li>
+                    <li><a href="#">About Us</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Our Works</a></li>
+            <li><a href="#">About Us</a></li>
+        </ul>
+    </nav>
+</header>
+<div id="banner" class="clearfix">
+	<h1>REMEMBER US FOR: WEB DEVELOPMENT, HOSTING, DOMAIN REGISTRATION</h1>
+    <div class="slider-if-any">
+    	<img src="images/banner.png" alt="Click Survey Solution Services">
+    </div>
+</div>
+
+
+<div id="content" class="clearfix">
+	<div class="services-lists">
+    	<h1>the technology</h1>
+        <ul>
+        	<li>
+            	<a href="#"><img src="images/web-development.jpg" alt="Web Development"></a>
+                <h4 class="red-ribbon">services</h4>
+                <h2>WEB DEVELOPMENT</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
+                <a href="#" class="readmore">READ MORE</a>
+            </li>
+            <li>
+            	<a href="#"><img src="images/hosting.jpg" alt="Web / Cloud Hosting"></a>
+                <h4 class="red-ribbon">services</h4>
+                <h2>HOSTING / CLOUD HOSTING</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
+                <a href="#" class="readmore">READ MORE</a>
+            </li>
+            <li class="no-margin">
+            	<a href="#"><img src="images/domain.jpg" alt="Domain Registration"></a>
+                <h4 class="red-ribbon">services</h4>
+                <h2>DOMAIN REGISTRATION</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
+                <a href="#" class="readmore">READ MORE</a>
+            </li>
+        </ul>
+    </div>
+</div>
+<footer>
+        <div id="footer-three-blocks" class="clearfix">
+            <div class="footer-widget-container">
+                <h1>About the Company</h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.  quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
+                <a href="#">Read More</a>
+            </div>
+            <div class="footer-widget-container">
+                <h1>subscribe with us</h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.  quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
+                <a href="#">Read More</a>
+            </div>
+            <div class="footer-widget-container no-margin">
+                <h1>CONTACT DETAILS</h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod..</p>
+                <div class="number-email">
+                	<span>T:</span> +1-9854125874<br>
+					<span>E:</span> info@clicksurvey.com
+                </div>
+                <div class="google-map-text">
+                	<a href="#">GOOGLE MAP</a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-menu">
+        	<ul class="footer-submenu">
+            	<li><a href="#">Terms</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <p id="copyright">Copyright and All Rights Reserved - clicksurveysolutions.com 2012</p>
+        </div>
+</footer>
+</body>
+</html>
